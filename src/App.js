47e74e42cec56a9b8d2c04f53406907f62d589e8
Vasan Subramanian <vasan.srini@gmail.com>
2015-12-07 15:12:09 +0530

    Cosmetic: removed extra punctuation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ var Hello = React.createClass({
 	render: function(){
 		return (
 			<div>
-			Hello <b>{this.props.message}</b>.
+			Hello <b>{this.props.message}</b>!
 			</div>
 			)
 	}
 });
 
-ReactDOM.render(<Hello message="World!"/>, document.getElementById('example'));
+ReactDOM.render(<Hello message="World"/>, document.getElementById('example'));
