dc1da4ed52f6117005212dbce5e88e1dd099d4f2
Vasan Subramanian <vasan.srini@gmail.com>
2015-12-07 18:34:14 +0530

    Using same App.js as in the first step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
-
-var Hello = React.createClass({
-	render: function(){
-		return (
-			<div>
-			Hello <b>{this.props.message}</b>!
-			</div>
-			)
-	}
-});
-
-ReactDOM.render(<Hello message="World"/>, document.getElementById('example'));
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('example')
+);
--- static/App.js
+++ static/App.js
@@ -1,26 +1,5 @@
-/*
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('example')
-);
-*/
-
-var Hello = React.createClass({
-	displayName: "Hello",
-
-	render: function () {
-		return React.createElement(
-			"div",
-			null,
-			"Hello ",
-			React.createElement(
-				"b",
-				null,
-				this.props.message
-			),
-			"."
-		);
-	}
-});
-
-ReactDOM.render(React.createElement(Hello, { message: "World!" }), document.getElementById('example'));
+ReactDOM.render(React.createElement(
+	'h1',
+	null,
+	'Hello, world!'
+), document.getElementById('example'));
