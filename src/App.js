1976fa1de425d749c5bbb6211d238495f9cc6adf
Vasan Subramanian <vasan.srini@gmail.com>
2015-12-08 18:09:39 +0530

    Step 4: composing components with data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
+var Bug = React.createClass({
+	render: function() {
+		return(
+			<tr className="bug">
+				<td className="bug-id">{this.props.data._id}</td>
+				<td className="bug-owner">{this.props.data.owner}</td>
+				<td className="bug-description">{this.props.data.title}</td>
+			</tr>
+		);
+	}
+});
+
+var BugList = React.createClass({
+	render: function() {
+		var bugs = this.props.data.map(function(bug) {
+			return (
+				<Bug key={bug._id} data={bug} />
+			);
+		});
+		return (
+			<table className="bug-list">
+				<thead>
+					<tr className="bug header">
+						<th className="bug-id">Id</th>
+						<th className="bug-owner">Owner</th>
+						<th className="bug-description">Title</th>
+					</tr>
+				</thead>
+				<tbody>
+					{bugs}
+				</tbody>
+			</table>
+		)
+	}
+});
+
+var allBugs = [
+	{_id: 1, owner: 'Vasan', title: '404 Not Found on some files'},
+	{_id: 2, owner: 'Sandeep', title: 'Error on console: no property "set" in undefined'},
+	{_id: 3, owner: 'Fazle', title: 'Warning: validateDOMNesting(...): <tr> cannot appear as a child of <table>.'}
+
+];
+
 ReactDOM.render(
-	<h1>Hello, World!</h1>,
+	<BugList data={allBugs}/>,
 	document.getElementById('example')
 );
+
--- static/App.js
+++ static/App.js
@@ -1,5 +1,71 @@
-ReactDOM.render(React.createElement(
-	'h1',
-	null,
-	'Hello, World!'
-), document.getElementById('example'));
+var Bug = React.createClass({
+	displayName: "Bug",
+
+	render: function () {
+		return React.createElement(
+			"tr",
+			{ className: "bug" },
+			React.createElement(
+				"td",
+				{ className: "bug-id" },
+				this.props.data._id
+			),
+			React.createElement(
+				"td",
+				{ className: "bug-owner" },
+				this.props.data.owner
+			),
+			React.createElement(
+				"td",
+				{ className: "bug-description" },
+				this.props.data.title
+			)
+		);
+	}
+});
+
+var BugList = React.createClass({
+	displayName: "BugList",
+
+	render: function () {
+		var bugs = this.props.data.map(function (bug) {
+			return React.createElement(Bug, { key: bug._id, data: bug });
+		});
+		return React.createElement(
+			"table",
+			{ className: "bug-list" },
+			React.createElement(
+				"thead",
+				null,
+				React.createElement(
+					"tr",
+					{ className: "bug header" },
+					React.createElement(
+						"th",
+						{ className: "bug-id" },
+						"Id"
+					),
+					React.createElement(
+						"th",
+						{ className: "bug-owner" },
+						"Owner"
+					),
+					React.createElement(
+						"th",
+						{ className: "bug-description" },
+						"Title"
+					)
+				)
+			),
+			React.createElement(
+				"tbody",
+				null,
+				bugs
+			)
+		);
+	}
+});
+
+var allBugs = [{ _id: 1, owner: 'Vasan', title: '404 Not Found on some files' }, { _id: 2, owner: 'Sandeep', title: 'Error on console: no property "set" in undefined' }, { _id: 3, owner: 'Fazle', title: 'Warning: validateDOMNesting(...): <tr> cannot appear as a child of <table>.' }];
+
+ReactDOM.render(React.createElement(BugList, { data: allBugs }), document.getElementById('example'));
